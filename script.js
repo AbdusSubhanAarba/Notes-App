5207a77322b287f2a700d4565b9da5f4f0d91705
Abdus Subhan Aarba <abdusubhan7@gmail.com>
2024-11-12 18:23:27 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+const notesContainer = document.querySelector(".notes-container");
+const createBtn = document.querySelector(".btn");
+
+function showNotes() {
+  const savedNotes = localStorage.getItem("notes");
+  if (savedNotes) {
+    notesContainer.innerHTML = savedNotes;
+  }
+}
+
+showNotes();
+
+function updateStorage() {
+  localStorage.setItem("notes", notesContainer.innerHTML);
+}
+
+createBtn.addEventListener("click", () => {
+  let inputBox = document.createElement("p");
+  let img = document.createElement("img");
+  inputBox.className = "input-box";
+  inputBox.setAttribute("contenteditable", "true");
+  img.src = "images/delete.png";
+
+  inputBox.appendChild(img);
+  notesContainer.appendChild(inputBox);
+
+  inputBox.addEventListener("keyup", updateStorage);
+});
+
+notesContainer.addEventListener("click", function (e) {
+  if (e.target.tagName === "IMG") {
+    e.target.parentElement.remove();
+    updateStorage();
+  }
+});
+
+document.addEventListener("keydown", (event) => {
+  if (event.key === "Enter") {
+    document.execCommand("insertLineBreak");
+    event.preventDefault();
+  }
+});
